6fa3ac87a7c4c1033c162922834eb1a3bcb81b80
Justin Chau <chausicle@gmail.com>
2020-06-17 15:34:54 -0700

    Updates

---
+++ precache-manifest.aeff7c753a6f8e89e643a035e2e0205e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4905bc9153f137e17ab9431bec264b10",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "91a5283cb624dbf105f4",
+    "url": "/seasons/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "002965e77321fb4cb0c2",
+    "url": "/seasons/static/js/2.23a786a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.23a786a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a5283cb624dbf105f4",
+    "url": "/seasons/static/js/main.18647cf4.chunk.js"
+  },
+  {
+    "revision": "7aa38b62a80534558f86",
+    "url": "/seasons/static/js/runtime-main.53d2d0be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/seasons/static/media/logo.5d5d9eef.svg"
+  }
+]);
