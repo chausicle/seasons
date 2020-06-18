3912cb8fe04b686eaf9e11ca3efe88f2161622f3
Justin Chau <chausicle@gmail.com>
2020-06-17 17:52:18 -0700

    Render season depending on latitude

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>SeasonDisplay</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  return (
+    <div>
+      <i className={`${"summer" ? "sun" : "snowflake"} icon`} />
+      <h1>
+        {season === "summer" ? "Let's hit the beach!" : "Burr, it's chilly!"}
+      </h1>
+      <i className={`${"summer" ? "sun" : "snowflake"} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
